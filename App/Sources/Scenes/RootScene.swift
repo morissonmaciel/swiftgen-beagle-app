438932344da4dc0bf1db28a9dca13f538d3e8d09
Môrisson Marcel <morissonmaciel@hotmail.com>
2023-01-15 13:46:16 -0300

    Basic example reinvented

--- App/Sources/Scenes/RootScene.swift
+++ App/Sources/Scenes/RootScene.swift
@@ -7,10 +7,37 @@
 
 import SwiftUI
 
+extension Color {
+    static func random() -> Color {
+        return Color(red: Double.random(in: 0...1), green: Double.random(in: 0...1), blue: Double.random(in: 0...1))
+    }
+}
+
 struct RootScene: View {
+    @State var rotationAngle = 0.0
+    @State var color = Color.primary
+    
     var body: some View {
-        BeagleLocalView(
-            contents: getContents(file: "BeagleSample"))
+        VStack {
+            Image(systemName: "timelapse")
+                .resizable()
+                .frame(width: 200, height: 200)
+                .foregroundColor(color)
+                .rotationEffect(.degrees(rotationAngle))
+            
+            Text("Basic iOS App with SwiftGen")
+                .padding()
+        }.onAppear {
+            withAnimation(.linear(duration: 1)
+                .speed(0.1).repeatForever(autoreverses: false)) {
+                    rotationAngle = 360.0
+                }
+            
+            withAnimation(.easeIn(duration: 1)
+                .speed(0.1).repeatForever(autoreverses: true)) {
+                    color = Color.random()
+                }
+        }
     }
 }
 
@@ -18,8 +45,7 @@ struct RootScene: View {
 
 struct RootScene_Previews: PreviewProvider {
     static var previews: some View {
-        BeagleConfig().config()
-        return RootScene()
+        RootScene()
     }
 }
 
---
+++ App/Sources/Util/ViewPreview.swift
@@ -0,0 +1,25 @@
+//
+//  ViewPreview.swift
+//  BasicApp
+//
+//  Created by Morisson Marcel on 15/01/23.
+//
+
+import UIKit
+import SwiftUI
+
+struct ViewPreview: UIViewRepresentable {
+    let viewBuilder: () -> UIView
+
+    init(_ viewBuilder: @escaping () -> UIView) {
+        self.viewBuilder = viewBuilder
+    }
+
+    func makeUIView(context: Context) -> some UIView {
+        viewBuilder()
+    }
+
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        // Not needed
+    }
+}
